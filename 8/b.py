394263ead98768481c00635692e55cb96f0ebc75
Marcel Blistein <marcel.blistein@gmail.com>
2020-12-08 21:00:59 +0100

    Optimize day 8/ part 2

--- 8/b.py
+++ 8/b.py
@@ -3,18 +3,22 @@
 """Part 2
 We may swap exactly one 'jmp' for a 'nop' or vice versa.
 Idea: try to swap the jmp/ nop encountered last before hitting a loop.
-If that does not work, backtrack further up the chain"""
+If that does not work, backtrack further up the chain
+
+Note that nodes are not removed from 'seen' when backtracking. We can do this
+because we only ever modify the field we currently stand on --> if a field
+leads to a cycle once, it will always lead to a cycle.
+This improves Time complexity from O(n**2) to O(n)."""
 
 
 import fileinput
 
 
 def sol(lines):
-    idx = acc = 0
     seen = set()
     n = len(lines)
 
-    def backtrack(idx, acc, retries) -> int:
+    def backtrack(idx, acc, retries):
         if idx in seen:
             return None  # found loop
         elif idx == n:
@@ -25,19 +29,16 @@ def sol(lines):
         val = int(num)
         if op == 'acc':
             res = backtrack(idx + 1, acc + val, retries)
-            seen.remove(idx)
             return res
         elif op == 'nop':
             res = backtrack(idx+1, acc, retries)
             if res is None and retries == 1:
                 res = backtrack(idx + val, acc, 0)  # try to change 'nop' to 'jmp'
-            seen.remove(idx)
             return res
         elif op == 'jmp':
             res = backtrack(idx + val, acc, retries)
             if res is None and retries == 1:
                 res = backtrack(idx + 1, acc, 0)  # try to change 'jmp' to 'nop'
-            seen.remove(idx)
             return res
         else:
             raise ValueError(f"Invalid instruction {lines[idx]}")

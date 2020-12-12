53ce448c00f65d48d67b475ab396aba4652e0191
Marcel Blistein <marcel.blistein@gmail.com>
2020-12-12 09:44:05 +0100

    Modify print of timing tools

--- tools.py
+++ tools.py
@@ -6,7 +6,7 @@ def time_execution(func):
         t1 = time.time()
         res = func(*args, **kwargs)
         t2 = time.time()
-        print(f"Execution took {t2 - t1}s\n\n")
+        print(f"Execution of {func.__name__} took {t2 - t1}s\n")
         return res
 
     return decorated
